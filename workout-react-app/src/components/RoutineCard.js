81e983694a3e576c3db80ff85346c6e391b5eb44
ccb1139 <calebbergen000@gmail.com>
2023-01-23 19:59:03 -0500

    not working

--- workout-react-app/src/components/RoutineCard.js
+++ workout-react-app/src/components/RoutineCard.js
@@ -1,16 +1,28 @@
 import React from 'react'
 // import the css file for this component in the adjacent folder
 import { Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 import '../css/RoutineCard.css'
 
-function RoutineCard({Title}) {
+function RoutineCard({Title, Activities}) {  
+  // const [activities, setActivities] = useState(Activities);
+
+  // useEffect(() => {
+  //   setActivities(Activities);
+  // }, [Activities]);
+  console.log(Activities);
+
   return (
-    <div className="routine-card col border text-center p-2 m-2 col-6">
+    <div className="routine-card col border text-center p-2 m-2 col-md-6 col-12 ">
         <div className="routine-card-header border-bottom ">
             <h3>{Title}</h3>
         </div>
-        <div class="routine-card-body">
-            <p>Card Body</p>
+        <div className="routine-card-body">
+          <ul>
+            {Activities?.map((activity, index) => (
+              <li key={index}>{activity}</li>
+            ))}
+          </ul>
         </div>
     </div>
   )
--- workout-react-app/src/pages/Routine.js
+++ workout-react-app/src/pages/Routine.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
+import { useState } from "react";
 import RoutineCard from "../components/RoutineCard";
 
+
+
 function Routine() {
+  const [routines, setRoutines] = useState();
+
+  const testrt = ["Chest flys", "Bench Press", "Pull Ups"]
+  setRoutines(["Chest flys", "Bench Press", "Pull Ups"]);
+
   return (
     <div className="container">
-      <div className="d-flex flex-wrap">
-        <RoutineCard Title={"Chest Day"} />
-        <RoutineCard Title={"Back Day"} />
+      <div className="d-flex flex-wrap flex-md-nowrap align-items-center justify-content-center">
+        <RoutineCard Title={"Chest Day"} Actvities={testrt} />
+        <RoutineCard Title={"Back Day"} Actvities={["Chest flys", "Bench Press", "Pull Ups"]} />
       </div>
     </div>
   );
