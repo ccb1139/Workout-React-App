4b103ab5c85f1f0a1c81562899b654bec20ee742
ccb1139 <calebbergen000@gmail.com>
2023-01-25 21:53:42 -0500

    Added new link

--- workout-react-app/src/pages/Home.js
+++ workout-react-app/src/pages/Home.js
@@ -5,6 +5,8 @@ function Home() {
     <div>
       <h1>Home</h1>
       <p>Tutorial can be found at: <a href='https://www.geeksforgeeks.org/how-to-build-a-basic-crud-app-with-node-js-and-reactjs/' >https://www.geeksforgeeks.org/how-to-build-a-basic-crud-app-with-node-js-and-reactjs/</a> </p>
+      <p>Grid thing use for fridge <a href='https://www.ag-grid.com/react-data-grid/cell-editing/'>HERE</a> </p>
+      <p>Spoon Tacular API <a href='https://spoonacular.com/food-api/docs'>https://spoonacular.com/food-api/docs</a></p>
     </div>
   )
 }
